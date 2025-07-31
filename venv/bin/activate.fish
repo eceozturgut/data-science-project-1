9f0ea7c7d78deb47f98974978aa0b491fd829867
eceozturgut <eceozturgut@hotmail.com>
2025-07-31 19:49:11 +0300

    Final: Tüm SQL sorguları tamamlandı ve password postgres olarak ayarlandı

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Ece/data-science-project-1/venv"
+set -gx VIRTUAL_ENV "/Users/Ece/Documents/GitHub/data-science-project-1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
